3c92bfbc41c55b9305d18472b0948a899e3b35b9
Rem Lampa <rem@remlampa.com>
2020-01-23 11:02:48 +0800

    Setup Heroku :wrench:

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn django_craigslist.wsgi
--- src/django_craigslist/settings.py
+++ src/django_craigslist/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 
 import os
 
+import django_heroku
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
@@ -119,3 +121,5 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'), )
+
+django_heroku.settings(locals())
